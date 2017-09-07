abb47e0111f5300b45e4f073effbb42bdeca9a18
Your Name <renzo-lim@hotmail.com>
2017-09-08 02:51:43 +1000

    Browser Script added and working footer links

--- navshrink.js
+++ navshrink.js
@@ -1,12 +1,38 @@
 $(document).ready(function() {
-	$(window).scroll(function() {
-  	if($(document).scrollTop() > 10) {
-    	$('#nav').addClass('shrink');
-      $('#L').addClass('show');
+
+	// Opera 8.0+
+	var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+	// Firefox 1.0+
+	var isFirefox = typeof InstallTrigger !== 'undefined';
+
+	// Safari 3.0+ "[object HTMLElementConstructor]"
+	var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+	// Internet Explorer 6-11
+	var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+	// Edge 20+
+	var isEdge = !isIE && !!window.StyleMedia;
+
+	// Chrome 1+
+	var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+	// Blink engine detection
+	var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+  	if(isChrome) {
+    	$('#browser').addClass('show-browser');
     }
-    else {
-    $('#nav').removeClass('shrink');
-    $('#L').removeClass('show');
-    }
-  });
+
+		$(window).scroll(function() {
+			if($(document).scrollTop() > 10) {
+				$('#nav').addClass('shrink');
+				$('#L').addClass('show');
+			}
+			else {
+			$('#nav').removeClass('shrink');
+			$('#L').removeClass('show');
+			}
+		});
 });
